63af4226e2c6bda425c3e482c44a43f6d95487e1
Jinwoo Nam <devnjw@gmail.com>
2024-02-03 16:01:40 +0900

    add FIXME

--- lambda-fastapi-dynamodb-cdk/cdk/lib/db-stack.ts
+++ lambda-fastapi-dynamodb-cdk/cdk/lib/db-stack.ts
@@ -3,6 +3,8 @@ import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 
+// FIXME: Lambda에서 사용하는 테이블은 이 테이블이 아니다.
+//        Lambda BuildArgs로 테이블 이름을 넘겨주는 방식으로 변경 필요.
 export class DbStack extends cdk.Stack {
   public readonly table: dynamodb.Table;
   
